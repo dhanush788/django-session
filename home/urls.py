ca8f7c890d7f146ab8f97df2a5f16039d655c9d9
dhanush788 <dhanushpk50@gmail.com>
2022-08-04 16:45:35 +0530

    made changes

--- home/urls.py
+++ home/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-
+from home.views import Todoset
 from . import views
 # Setup the URLs and include login URLs for the browsable API.
 from .views import index
 
 router = DefaultRouter()
 
+router.register('todo',Todoset)
 urlpatterns = [
-    path(r'', index)
+    path(r'', include(router.urls))
 ]
